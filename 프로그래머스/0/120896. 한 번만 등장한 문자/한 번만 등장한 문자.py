a4c0dc5045510fbd1804121738685f792fb9f3a2
woos-02 <wooseong4954@gmail.com>
2025-07-25 19:37:48 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,12 +1,6 @@
 def solution(s):
     answer = ''
-    count_lst = [0] * 26
-    
-    for str in s:
-        count_lst[ord(str) - 97] += 1
-    
-    for idx in range(len(count_lst)):
-        if count_lst[idx] == 1:
-            answer += chr(idx + 97)
-    
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        if s.count(c) == 1:
+            answer += c
     return answer
