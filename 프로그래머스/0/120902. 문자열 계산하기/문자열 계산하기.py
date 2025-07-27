1543b929159a130f11c6a9fe755667b105f45964
woos-02 <wooseong4954@gmail.com>
2025-07-27 16:10:38 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,14 +1,2 @@
 def solution(my_string):
-    tokens = my_string.split()
-    answer = int(tokens[0])
-    
-    for i in range(1, len(tokens), 2):
-        op = tokens[i]
-        num = int(tokens[i + 1])
-        
-        if op == '+':
-            answer += num
-        elif op == '-':
-            answer -= num
-    
-    return answer
+    return eval(my_string)
