5f117d33b07a4d9e27d7c964c95851dd98663d10
woos-02 <wooseong4954@gmail.com>
2025-07-27 16:28:19 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,10 +1,2 @@
 def solution(numbers, k):
-    answer = 0
-    idx = 0
-    round_numbers = numbers * k
-    
-    for i in range(1, k + 1):
-        answer = round_numbers[idx]
-        idx += 2
-
-    return answer
+    return numbers[2 * (k - 1) % len(numbers)]
