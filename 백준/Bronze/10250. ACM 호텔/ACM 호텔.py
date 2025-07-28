faacac60a619db4721afb6a37bff6ffbc893ce30
woos-02 <wooseong4954@gmail.com>
2025-07-28 23:38:08 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,10 @@
+T = int(input())
+for i in range(T):
+    H, W, N = map(int, input().split())
+    floor = N % H
+    room = N // H + 1
+    if floor == 0:
+        room -= 1
+        floor = H
+        
+    print(floor*100+room)
