f02f8398f0452c73f5481552f2660a89c4a43262
woos-02 <wooseong4954@gmail.com>
2025-07-28 18:55:38 +0900

    [Bronze IV] Title: 상근날드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,8 @@
+sd = int(input())
+jd = int(input())
+hd = int(input())
+coke = int(input())
+cider = int(input())
+bg_list = [sd, jd, hd]
+drink_list = [coke, cider]
+print(min(bg_list)+min(drink_list)-50)
