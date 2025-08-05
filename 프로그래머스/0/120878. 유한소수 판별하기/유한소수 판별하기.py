887152648d80d61f2a4957811ad1b6ed148069da
woos-02 <wooseong4954@gmail.com>
2025-08-05 19:10:53 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.07 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -16,3 +16,19 @@ def solution(a, b):
     if all(i in [2,5] for i in num): # 소인수가 2와 5만 존재하면 유한소수 그렇징 않으면 무한소수
         return 1
     return 2
+
+# def solution(a, b):
+#     answer = 0
+#     for i in range(2, min([a, b]) + 1):
+#         while a % i == 0 and b % i == 0:
+#             a = a // i
+#             b = b // i
+#     while b % 2 == 0:
+#         b = b // 2
+#     while b % 5 == 0:
+#         b = b // 5
+#     if b == 1:
+#         answer = 1
+#     else:
+#         answer = 2
+#     return answer
