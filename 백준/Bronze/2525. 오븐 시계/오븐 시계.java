56c2a111a1370f5afea59022525f28548269837e
LEE WOOSEONG <wooseong4954@gmail.com>
2025-09-07 17:44:37 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int a = sc.nextInt();
+        
+        int min = h * 60 + m;
+        min += a;
+
+        int hour = (min/60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
+        
+    }
+}
