428a1e0d447c9b2628417441536af1e4839feb43
LEE WOOSEONG <wooseong4954@gmail.com>
2025-09-07 21:05:25 +0900

    [Bronze I] Title: 평균, Time: 312 ms, Memory: 27388 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double arr[] = new double[sc.nextInt()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextDouble();
+        }
+        sc.close();
+
+        double sum = 0;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += ((arr[i] / arr[arr.length - 1]) * 100);
+        }
+        System.out.print(sum / arr.length);
+    }
+}
