ec66436fe4b5e9073bec6ba386e449be0e14c108
LEE WOOSEONG <202412518+woos-02@users.noreply.github.com>
2025-09-11 23:05:45 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 168 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int dot = 2;
+
+        for (int i = 0; i < n; i++) {
+            dot += (dot - 1);
+        }
+        System.out.println(dot * dot);
+
+        sc.close();
+    }
+}
